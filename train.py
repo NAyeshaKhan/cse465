3b9149d94cce4bb6c9a0c705e972b3c062369419
Ramisa Rifa <ramisarifa@Ramisas-MacBook-Pro.local>
2022-08-25 22:54:33 +0600

    train.py added into repo

---
+++ train.py
@@ -0,0 +1,9 @@
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+from utils import save_checkpoint, load_checkpoint, print_examples
+from get_loader import get_loader
+from model import CNNtoRNN
